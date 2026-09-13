e9f926391cc3875102e6af3076acf9fcbb9c6933
adwait768 <adwaitvsuryawanshi1@gmail.com>
2026-09-13 20:35:34 +0530

    Add Android project source to repository root

--- gradlew.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-SET DIRNAME=%~dp0
-IF "%DIRNAME%" == "" SET DIRNAME=.
-SET APP_HOME=%DIRNAME%
-"%JAVA_HOME%\bin\java.exe" -Dorg.gradle.appname=gradlew -classpath "%APP_HOME%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
