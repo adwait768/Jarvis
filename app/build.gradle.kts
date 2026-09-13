38fc82e554640cd6110832a3520f253838a7f1f8
adwait768 <adwaitvsuryawanshi1@gmail.com>
2026-09-13 20:52:39 +0530

    Align Android compilation with JVM 17

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.adwait.jarvis"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.adwait.jarvis"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
